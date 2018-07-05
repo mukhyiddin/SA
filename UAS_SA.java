2c501ca54c748aa2737cb62de759533c6fc4d1ea
mukhyiddin <39786246+mukhyiddin@users.noreply.github.com>
2018-07-05 12:36:35 +0700

    Create UAS_SA.java

---
+++ UAS_SA.java
@@ -0,0 +1,25 @@
+public class UAS_SA { 
+
+boolean solveMaze(int maze[][])
+{
+    int sol[][] = {{0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    if (solveMazeUtil(maze, 0, 0, sol) == false)
+    {
+        System.out.print("Solution doesn't exist");
+        return false;
+    }
+
+    printSolution(sol);
+    return true;
+}
+
+boolean solveMazeUtil(int maze[][], int x, int y,
+                      int sol[][])
+{ }                      
+
+}
